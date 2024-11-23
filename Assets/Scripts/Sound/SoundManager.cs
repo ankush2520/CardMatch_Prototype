f0f0f6733a189ef5509f1e6041b001056dde516b
Ankush Sharma <ankush@webzool.com>
2024-11-23 14:59:32 +0530

    Sound Class Added
    Class added for sound management

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CardMatch
+{
+    public class SoundManager : MonoBehaviour
+    {
+        [SerializeField] AudioSource audioSource;
+        [SerializeField] AudioClip flipSound;
+
+        public void PlayFlipSound() {
+           
+           // audioSource.Pla (flipSound);
+        }
+    }
+}
